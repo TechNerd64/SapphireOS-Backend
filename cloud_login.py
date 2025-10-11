97c2e4f1c0aea755c3924071e246d68c45ef0358
TechNerd64 <jaxonhohns@gmail.com>
2025-10-10 21:14:40 -0500

    First Commit

---
+++ cloud_login.py
@@ -0,0 +1,20 @@
+import scratchattach as sa
+from dotenv import load_dotenv
+import os
+
+#Load .env variables
+load_dotenv()
+sessionID = os.getenv("sessionID")
+USERNAME = os.getenv("username")
+
+session = sa.login_by_id(sessionID, username= USERNAME)
+print("Logged in as " + USERNAME)
+
+cloud = session.connect_cloud("1227635188") #Test Program, NOT SappireOS
+print("Connected to cloud project: ", cloud)
+
+value = cloud.get_var("Cloud1")
+
+
+value = cloud.get_var("Cloud1")
+print("Cloud1 value is: " + str(value))
