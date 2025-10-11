ce8b7d022bcd331581837b5b029b6e8a0d8e930a
TechNerd64 <jaxonhohns@gmail.com>
2025-10-11 14:05:44 -0500

    Add user data request handler to cloud client

--- cloud_and_db.py
+++ cloud_and_db.py
@@ -20,6 +20,13 @@ def ping():
     return "Pong!  This works great!"
 
 @client.request
+def send_user_data(argument1, argument2):
+    print("User data request received!")
+    print("Argument 1: ", argument1)
+    print("Argument 2: ", argument2)
+    return f"Received arguments: {argument1}, {argument2}"
+
+@client.request
 def on_ready():
     print("Request handler is running!")
 
